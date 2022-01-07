1267102e6da703fd81ff2405f337dbd23fd645b1
salrey <tekdehn@gmail.com>
2022-01-07 17:19:42 -0500

    Send json and add error handling, status codes

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const bookmarksController = require("./controllers/bookmarksController")
 // CONFIGURATION
 const app = express();
 
+// ROUTES
+app.get("/", (request, response) => {
+    console.log("GET request received to route: /")
+    response.send("Welcome to Bookmarks App");
+});
+
 // Delegate everything that starts with /bookmarks to the bookmarks controller
 // .use takes two arguments the route, 
 // - the subroute for the controller to handle  
@@ -14,11 +20,12 @@ const app = express();
 //For any route that starts with /bookmarks, use the bookmarks controller
 app.use("/bookmarks", bookmarksController)
 
-// ROUTES
-app.get("/", (request, response) => {
-    console.log("GET request received to route: /")
-    response.send("Welcome to Bookmarks App");
-});
+//ERROR Handling
+// Star (*) matches anything we haven't matched in previous routes yet
+app.get("*", (request, response) => {
+    //The right way to do this is o send it on an object that way when you do on the front end, fetch request response that JSON, you do a quick check. Is there a key called an error on the object you just sent me. 
+    response.status(404).json({ error: "Page not found"})
+})
 
 // EXPORT our app for server.js
 module.exports = app;
--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -1,5 +1,6 @@
 //Dependencies 
 const express = require("express")
+const app = require("../app")
 // .Router creates a new controller that handles a subroute. 
 // In this case, it will handle everything that starts with /bookmarks
 // bookmarks is like a subApp that handles one route
@@ -11,8 +12,11 @@ const bookmarksList = require("../models/bookmark")
 //ROUTES 
 // "/bookmarks" is already assumed because app.js has delegated it to us. 
 bookmarks.get("/", (request, response) => {
-    response.send(bookmarksList)
+    //We actually want to sent JSON vs the bookmarks array
+    //***USE JSON.stringify(array) or response.json(array)
+    response.json(bookmarksList)
 })
+
 
 //Export the bookmarks controller/router so that app can delegate the /bookmarks route to it
 module.exports = bookmarks;
