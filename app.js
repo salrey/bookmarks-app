07dc6364b6d15b028ec390d3b205dd20265f4804
salrey <tekdehn@gmail.com>
2022-01-11 13:01:06 -0500

    POST to bookmarks and error handling

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ const bookmarksController = require("./controllers/bookmarksController")
 // CONFIGURATION
 const app = express();
 
-// ROUTES
-app.get("/", (request, response) => {
-    console.log("GET request received to route: /")
-    response.send("Welcome to Bookmarks App");
-});
+//MIDDLEWARE 
+
+// JSON parsing middleware
+// For every request, parse incoming information as JSON
+// So use our JSON parser
+// So that here in our terminal. we don't have to keep having our GET requests logged right all these GET requests we logged ourselves will use middleware to do that for us. Eventually, but this is what we actually want this time we want a jSON parsing middleware that takes that JSON and handles that
+app.use(express.json())
+
+// In the middle of every request, check if the endpoint starts with /bookmarks 
+// If so send it to the bookmarks controller router
+// Before we get this request, bookmarks controller is beign used by express
+// This runs in the middle 
 
 // Delegate everything that starts with /bookmarks to the bookmarks controller
 // .use takes two arguments the route, 
@@ -19,6 +26,13 @@ app.get("/", (request, response) => {
 // - which controller should handle it
 //For any route that starts with /bookmarks, use the bookmarks controller
 app.use("/bookmarks", bookmarksController)
+
+
+// ROUTES
+app.get("/", (request, response) => {
+    console.log("GET request received to route: /")
+    response.send("Welcome to Bookmarks App");
+});
 
 //ERROR Handling
 // Star (*) matches anything we haven't matched in previous routes yet
--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -23,15 +23,39 @@ bookmarks.get("/", (_, response) => {
 })
 
 // GET ONE BOOKMARK BY INDEX
+// TIP: Send as a json
+// BONUS - send a 404 *if* the index doesn't exist in our bookmarksList
 bookmarks.get("/:index", (request, response) => {
     const { index } = request.params
     console.log("GET request to /:index")
-    //ERROR HANDLING  
+    //ERROR HANDLING - status codes 
     if (bookmarksList[index]) {
         response.json(bookmarksList[index])
     } else {
         response.status(404).json({error: "Bookmark not found"})
     }
+})
+
+// Create one bookmark
+// POST to /bookmarks (which is already there thanks to app.js delegating it to us)
+    // if there's three things in list, you want to post to index 3
+    // On the backend it's much easier to find out where to POST by simply pushing into the array
+// A request to POST /bookmarks will need to include WHAT to ADD
+// POST to /bookmarks... but post WHAT? post THIS to bookmarks
+bookmarks.post("/", (request, response) => {
+    console.log(request.body)
+    //request.body does not handle json. It's handling middleware.
+    //We are going to say, hey, app. js, whenever a request comes in. Here's a purse or for JSON to handle.
+    // every server is going to need this, it's going to need some way to handle JSON there are different ways to handle JSON, but Express comes with a built in way to handle JSON,
+    // curl -H (Header to add to request)"Content-Type: application/json" -X (HTML verb get post, patch, delete) POST -d (my data, the body of my request) '{"name":"AV Club", "url": "https://www.avclub.com"}' (the argument to curl is what URL to hit) localhost:3003/bookmarks
+    // response.send(request.body)
+
+    //GET the new bookmark on request.body into our array
+    //Send back the whole bookmarks array as json
+    //BONUS: Send a 201 ("Created") HTTP status code
+        // it means yes we successfully created it will not have you do error, any error handling on this one because they really talked about how to handle what can go wrong with a post.
+        bookmarksList.push(request.body)
+        response.status(201).json(bookmarksList[bookmarksList.length - 1])
 })
 
 //Export the bookmarks controller/router so that app can delegate the /bookmarks route to it
