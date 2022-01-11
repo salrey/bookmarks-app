2ecd68165afa836bd1e043be14f33f3b93dc9e5d
salrey <tekdehn@gmail.com>
2022-01-11 12:11:21 -0500

    Get specific index and error handling

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -11,12 +11,28 @@ const bookmarksList = require("../models/bookmark")
 
 //ROUTES 
 // "/bookmarks" is already assumed because app.js has delegated it to us. 
-bookmarks.get("/", (request, response) => {
-    //We actually want to sent JSON vs the bookmarks array
+// The underscore in place of request parameter means that request is not being used 
+    // bookmarks.get("/", (_, response) => {
+
+// GET ALL BOOKMARKS
+bookmarks.get("/", (_, response) => {
+    //We actually want to send JSON vs the bookmarks array
     //***USE JSON.stringify(array) or response.json(array)
+    console.log("GET request to /bookmarks")
     response.json(bookmarksList)
 })
 
+// GET ONE BOOKMARK BY INDEX
+bookmarks.get("/:index", (request, response) => {
+    const { index } = request.params
+    console.log("GET request to /:index")
+    //ERROR HANDLING  
+    if (bookmarksList[index]) {
+        response.json(bookmarksList[index])
+    } else {
+        response.status(404).json({error: "Bookmark not found"})
+    }
+})
 
 //Export the bookmarks controller/router so that app can delegate the /bookmarks route to it
 module.exports = bookmarks;
